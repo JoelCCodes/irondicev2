059a738b5c39974f5e491f8d5c4d4f19050d1504
JoelCCodes <joelce23@gmail.com>
2022-02-26 15:19:51 -0500

    ...

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,48 +1,309 @@
+import path from 'path'
+
+/* eslint-disable prefer-const */
+let { DEPLOYED_NET_ENV, NETWORK_ID, NODE_ENV } = process.env
+/* eslint-enable prefer-const */
+
+console.log({ DEPLOYED_NET_ENV, NODE_ENV })
+
+/**
+ * const isMainnet   = NODE_ENV === 'production' && DEPLOYED_NET_ENV  === 'mainnet'
+ * const isTestnet   = NODE_ENV === 'production' && DEPLOYED_NET_ENV  === 'testnet'
+ * const isTestnet2  = NODE_ENV === 'production' && DEPLOYED_NET_ENV  === 'testnet2
+ * const development = NODE_ENV === 'development' && DEPLOYED_NET_ENV === 'local'
+ *
+ * production  === app.ciphershooters.io     === mainnet
+ * development === dev.ciphershooters.io     === testnet2 (before we hit mainnet)
+ * uat         === staging.ciphershooters.io === testnet (currently where users test)
+ * local       === localhost:3000            === local eth truffle development / ganache-cli
+ */
+
+// DEPRECATED serves as uat via staging.ciphershooters.io
+// if (DEPLOYED_NET_ENV === 'testnet') {
+//   NETWORK_ID = '80001'
+//   IRONDICE_CONTRACT =
+//     IRONDICE_CONTRACT || '0x6F6dA672be82bc400198A6bFF21d22B31D013b92'
+//   TAGS_TOKEN_CONTRACT =
+//     TAGS_TOKEN_CONTRACT || '0xc69F4eF2138764A52e7dd7Ec2931d1CdD7B32d0f'
+// }
+
+const networkData =
+  DEPLOYED_NET_ENV === 'mainnet'
+    ? [
+        {
+          // 137
+          chainId: '0x89',
+          chainName: 'Polygon',
+          rpcUrls: ['https://polygon-rpc.com/'],
+          socketRpcUrls: ['wss://matic-mainnet-full-ws.bwarelabs.com'],
+          nativeCurrency: {
+            name: 'Matic',
+            symbol: 'MATIC',
+            decimals: 18,
+          },
+          blockExplorerUrls: ['https://polygonscan.com/'],
+        },
+      ]
+    : DEPLOYED_NET_ENV === 'testnet' || DEPLOYED_NET_ENV === 'testnet2'
+    ? [
+        {
+          // 80001
+          chainId: '0x13881',
+          chainName: 'Matic Mumbai Testnet',
+          rpcUrls: ['https://matic-mumbai.chainstacklabs.com/'],
+          socketRpcUrls: ['wss://ws-matic-mumbai.chainstacklabs.com'],
+          nativeCurrency: {
+            name: 'Matic',
+            symbol: 'MATIC',
+            decimals: 18,
+          },
+          blockExplorerUrls: ['https://mumbai.polygonscan.com//'],
+        },
+      ]
+    : [
+        {
+          // 1337
+          chainId: '0x539',
+          chainName: 'Truffle Development Local Net',
+          rpcUrls: ['https://127.0.0.1:9545'],
+          nativeCurrency: {
+            name: 'Ethereum',
+            symbol: 'ETH',
+            decimals: 18,
+          },
+        },
+      ]
+
+const baseURL = 'https://localhost:3000'
+
+console.log('\n')
+console.log('baseURL: ', baseURL)
+console.log('NETWORK_ID: ', NETWORK_ID)
+
+console.log('\n')
+
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  generate: {
+    fallback: true,
+  },
+
+  loading: {
+    color: 'white',
+    height: '5px',
+  },
+
+  loadingIndicator: {
+    name: 'pulse',
+    color: '#fff',
+    background: 'rgba(81, 39, 197, 0.1)',
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'irondice',
+    title: 'IronDice - Heavy Rolls for Heavy Hitters',
     htmlAttrs: {
       lang: 'en',
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+    link: [
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href: '/augmented-ui.min.css',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [
+      {
+        src: 'https://code.jquery.com/jquery-3.6.0.min.js',
+        integrity: 'sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=',
+        crossorigin: 'anonymous',
+        type: 'text/javascript',
+        charset: 'utf-8',
+      },
+      {
+        src: 'https://cdnjs.cloudflare.com/ajax/libs/tilt.js/1.2.1/tilt.jquery.min.js',
+        type: 'text/javascript',
+        charset: 'utf-8',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['element-ui/lib/theme-chalk/index.css'],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/element-ui'],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+  plugins: [
+    // MAINTAINED ORDER START
+    // { src: '~/plugins/web3Modal.js', mode: 'client' },
+    // { src: '~/plugins/web3.js', mode: 'client' },
+    // { src: '~/plugins/contracts.js', mode: 'client' },
+    // // MAINTAINED ORDER END
+    // { src: '~/plugins/gasStations', mode: 'client' },
+    // { src: '~/plugins/vue-particles.js', mode: 'client' },
+    // { src: '~/plugins/vue-number.js', mode: 'client' },
+    // { src: '~/plugins/typeit.js', mode: 'client' },
+    // { src: '~/plugins/nft-storage-client.js', mode: 'client' },
+    // { src: '~/plugins/nft-storage-file.js', mode: 'client' },
+    // { src: '~/plugins/vuex-persist', mode: 'client' },
   ],
 
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: [
+    {
+      path: '~/components', // will get any components nested in let's say /components/test too
+      pathPrefix: false,
+    },
+  ],
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    // https://go.nuxtjs.dev/stylelint
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [],
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
-  },
+  axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: [/^element-ui/],
+    standalone: true,
+    plugins: [
+      // new webpack.ProvidePlugin({
+      //   // $: 'jquery',
+      // }),
+    ],
+    splitChunks: {
+      layouts: true,
+    },
+    extractCSS: {
+      ignoreOrder: true,
+    },
+    optimization: {
+      runtimeChunk: true,
+      splitChunks: {
+        cacheGroups: {
+          styles: {
+            name: 'styles',
+            test: /\.(css|scss|sass|vue)$/,
+            chunks: 'all',
+            enforce: true,
+          },
+        },
+      },
+    },
+    html: {
+      minify: {
+        collapseBooleanAttributes: true,
+        decodeEntities: true,
+        minifyCSS: true,
+        minifyJS: true,
+        processConditionalComments: true,
+        removeEmptyAttributes: true,
+        removeRedundantAttributes: true,
+        trimCustomFragments: true,
+        useShortDoctype: true,
+      },
+    },
+    transpile: ['vee-validate'],
+    // postcss: {
+    //   plugins: {
+    //     'postcss-custom-properties': false,
+    //     tailwindcss: path.resolve(__dirname, '../tailwind.config.js'),
+    //     'postcss-pxtorem': {
+    //       propList: ['*', '!border*'],
+    //     },
+    //   },
+    // },
+    babel: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              esmodules: true,
+            },
+            loose: true,
+            shippedProposals: true,
+          },
+        ],
+      ],
+    },
   },
+
+  /**
+   * const isMainnet   = NODE_ENV === 'production' && DEPLOYED_NET_ENV  === 'mainnet'
+   * const isTestnet   = NODE_ENV === 'production' && DEPLOYED_NET_ENV  === 'testnet'
+   * const isTestnet2  = NODE_ENV === 'production' && DEPLOYED_NET_ENV  === 'testnet2
+   * const development = NODE_ENV === 'development' && DEPLOYED_NET_ENV === 'local'
+   *
+   * TODO:
+   * production === app.ciphershooters.io === mainnet
+   * development === dev.ciphershooters.io === testnet
+   * staging === stage.ciphershooters.io === testnet2
+   * local === localhost:3000 === local eth truffle development / ganache-cli
+   */
+  publicRuntimeConfig: {
+    /**
+     * access in app:
+     *
+     *      this.$config.NETWORK_ID
+     *
+     * https://nuxtjs.org/docs/2.x/directory-structure/nuxt-config#runtimeconfig
+     *
+     */
+
+    baseURL,
+    networkData,
+    NODE_ENV,
+    DEPLOYED_NET_ENV,
+    NETWORK_ID,
+  },
+  privateRuntimeConfig: {
+    DEPLOYED_NET_ENV,
+  },
+  // https://pwa.nuxtjs.org/
+  pwa: {
+    workbox: false,
+    manifest: {
+      name: 'CipherShooters',
+      short_name: 'CS',
+    },
+    meta: {
+      viewport: 'width=device-width, initial-scale=0.9',
+      appleStatusBarStyle: 'black-translucent',
+      nativeUI: true,
+      name: 'CipherShooters',
+      author: 'Humble DAO',
+      description: 'Truly sustainable Play and Earn Cyberpunk RPG Battler',
+      ogTitle: 'CipherShooters',
+      ogDescription: 'Truly sustainable Play and Earn Cyberpunk RPG Battler',
+      ogHost: baseURL,
+      ogImage: '/CypherShooters_Logo_9.png',
+    },
+  },
+
+  sitemap: {
+    hostname: baseURL,
+    gzip: true,
+  },
+
+  srcDir: '/',
+
+  rootDir: './',
+
+  tailwindcss: {
+    cssPath: '~/assets/styles/tailwind.css',
+    configPath: '~/../tailwind.config.js',
+    exposeConfig: true,
+    config: {},
+  },
+
+  router: {},
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,139 @@
 <template>
-  <Tutorial />
+
+  <div class="flex flex-col w-full">
+    <div class = "flex flex-row">
+    <div :show="true" text="6X Gains" class= "grid-cols-4 h-full w-1/3">
+    </div>
+    <div :show="false" text="Take your chance" icon="dice" class="grid-cols-4 h-full w-1/3">
+      <b-field label="Predicted Dice Outcome">
+        <b-numberinput v-model="diceOutcome" placeholder="1-6" :min="1" :max="6"></b-numberinput>
+      </b-field>
+      <b-field label="Bet Amount">
+        <b-numberinput v-model="betAmount" placeholder="2-500" :min="2" :max="500"></b-numberinput>
+      </b-field>
+      <center>
+        <b-button @click="placeBet"> Roll </b-button>
+      </center>
+    </div>
+    <div class="grid-cols-4 h-full w-1/3" text="Stats">
+      <h4 class="subtitle">
+        Last Number Rolled: {{ rolledValue }}
+        <br />
+        Total Bets: {{ totalBets }}
+        <br />
+        Total Rewards: {{ totalRewards }}
+        <br />
+        Bet Amount {{ betAmount }}
+        <br />
+        Dice Outcome: {{ diceOutcome }}
+        <br />
+      </h4>
+    
+    </div>
+    </div>
+    <b-table v-if="battleData" class="" :data="battleData" :columns="battleColumns"/></b-table>
+  </div>
+
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
-  name: 'IndexPage',
+  name: 'App',
+  data() {
+    return {
+      rolledValue: 1,
+      totalBets: 0,
+      totalRewards: 0,
+      betAmount: 2,
+      diceOutcome: 1,
+      battleData:[],
+      battleColumns: [
+           {
+          field: 'houseRoll',
+          label: 'House Roll',
+          numeric: true,
+        },
+        {
+          field: 'playerNumber',
+          label: 'Player Number',
+          numeric: true,
+        },
+        {
+            field: 'winnings',
+            label:"winnings",
+            numeric: true
+        }
+      ]
+    }
+  },
+  computed: {
+    ...mapState(['selectedAccount', 'balance']),
+  },
+  async mounted() {
+
+    console.log(this.$web3)
+    // await this.load()
+    // try {
+    //     const latestSubMax = (await this.$web3.eth.getBlockNumber()) - 3000
+    //     await this.$contracts.IronDice.events
+    //       .DiceRolled({
+    //         fromBlock: latestSubMax,
+    //         filter: {
+    //           owner: this.selectedAccount,
+    //         },
+    //       })
+    //       .on('data', function (event) {
+    //         this.battleData = events.map((a) => a.returnValues)
+    //         console.log(this.battleData, "Game Outcome")
+    //         console.log(event) // same results as the optional callback above
+    //       })
+    //     this.load()
+        
+    // } catch (error) {
+    //     console.log(error)
+    // }
+  },
+  methods: {
+    // async load() {
+    //   this.rolledValue = await this.$contracts.IronDice.methods.lastDiceRoll().call()
+    //   this.totalBets = await this.$contracts.IronDice.methods.totalBets().call()
+    //   this.totalRewards = (await this.$contracts.IronDice.methods.totalRewards().call()) / 10 ** 18
+    //   this.getSettledBets()
+    // },
+    // async placeBet() {
+    //   // await this.approval()
+    //   try {
+    //     let betAmountCTAG = this.$web3.utils.toWei(String(this.betAmount))
+    //     console.log(betAmountCTAG)
+    //     let res = await this.$contracts.IronDice.methods.placeBet(betAmountCTAG, this.diceOutcome).send({
+    //       from: this.selectedAccount,
+    //     })
+    //     console.log(res)
+    //   } catch (error) {
+    //     console.log(error)
+    //   } finally {
+    //     this.load()
+    //   }
+    // },
+    // async approval() {
+    //   await this.$contracts.TagToken.methods.approve(this.$contracts.IronDice._address, String(100 * 10 ** 18)).send({
+    //     from: this.selectedAccount,
+    //   })
+    // },
+    // async getSettledBets() {
+    //   try {
+    //     const latestSubMax = (await this.$web3.eth.getBlockNumber()) - 3000
+    //     const events = await this.$contracts.IronDice.getPastEvents('DiceRolled', {
+    //       filter: { owner: this.selectedAccount },  
+    //       fromBlock: latestSubMax,
+    //     })
+    //     this.battleData = events.map((a) => a.returnValues)
+    //     console.log(events)
+    //   } catch (e) {
+    //     console.log(e)
+    //   }
+    // },
+  },
 }
 </script>
