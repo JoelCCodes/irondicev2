26c0c673cd5ace7dc9d100b358b65bac6d92dab9
JoelCCodes <joelce23@gmail.com>
2022-02-26 15:46:18 -0500

    ...

--- plugins/web3.js
+++ plugins/web3.js
@@ -1,27 +1,10 @@
 import Web3 from 'web3'
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-export default function ({ $config: { networkData } }, inject) {
-  try {
-    const web3 = new Web3(networkData[0].rpcUrls[0])
-    const web3Socket = new Web3(
-      new Web3.providers.WebsocketProvider(networkData[0].socketRpcUrls[0]),
-      {
-        // Enable auto reconnection
-        reconnect: {
-          auto: true,
-          delay: 5000, // ms
-          maxAttempts: 20,
-          onTimeout: false,
-        },
-      }
-    )
-    console.log('INIT_WEB3: ', web3)
-    inject('web3', web3)
-  } catch (error) {
-    console.warn(
-      'web3 or web3socket initialization failed: ',
-      new Web3(networkData[0].rpcUrls[0])
-    )
-  }
+export default function (context, inject) {
+  // pass null as provider to not force connection during plugin instantiation
+  // as it will be set via web3.setProvider via an action
+  const web3 = new Web3(null)
+
+  inject('web3', web3)
 }
